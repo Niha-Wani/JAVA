44837a9fb8bd9544ba2d188d4538d9f5d9bbc6b0
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-13 20:42:46 +0530

    Add files via upload

---
+++ Factit.java
@@ -0,0 +1,17 @@
+package javaprograms;
+import java.util.*;
+public class Factit {
+
+	public static void main(String[] args) {
+		int fac=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value");
+		int a=sc.nextInt();
+		for(int i=1;i<=a;i++)
+		{
+			fac=i*fac;
+		}
+		System.out.println("The factorial of a number is" +fac);
+	}
+
+}
