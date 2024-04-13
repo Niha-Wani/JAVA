dd5a448ab9d43e49785acab60f9f217a754bd717
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-13 20:35:32 +0530

    Add files via upload

---
+++ Floatingint.java
@@ -0,0 +1,11 @@
+package javaprograms;
+import java.util.*;
+public class Floatingint {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the floating number ");
+	float x=sc.nextFloat();
+	System.out.println("After typre casting the number is " +(int)x);	
+	}
+
+}
