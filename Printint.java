2f347902ef43a83084749e2546697fc8d145dbee
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-13 08:04:12 -0700

    Update Printint.java

--- Printint.java
+++ Printint.java
@@ -2,11 +2,11 @@ package javaprograms;
 import java.util.Scanner;
 public class Printint
 {
-			public static void main(String[] args)
-			{
-				Scanner sc=new Scanner(System.in);
-				System.out.println("Enter the number");
-				int num=sc.nextInt();
-				System.out.println("The number entered is" +num);
+    public static void main(String[] args)
+      {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+	        int num=sc.nextInt();
+		System.out.println("The number entered is" +num);
 	}
 }
