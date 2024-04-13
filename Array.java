aab72df5124c14415c41ec5a15ebe0b3040c2eeb
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-12 21:50:26 -0700

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,14 +1,13 @@
 package javaprograms;
 import java.util.Scanner;
 public class Array {
-
 	public static void main(String[] args)
 	{
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter array size");
 		int size=s.nextInt();
 		int ar[]=new int [size];
-		System.out.println("enter array elemnets");
+		System.out.println("Enter array elements ");
 		for(int i=0;i<size;i++)
 		{
 			ar[i]=s.nextInt();
