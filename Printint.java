83a96a2929482d1faa172314953dcdcfd2c46251
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-13 20:32:34 +0530

    Add files via upload

---
+++ Printint.java
@@ -0,0 +1,12 @@
+package javaprograms;
+import java.util.Scanner;
+public class Printint
+{
+			public static void main(String[] args)
+			{
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter the number");
+				int num=sc.nextInt();
+				System.out.println("The number entered is" +num);
+	}
+}
