c8a8eab70c9af83bfdd9e12f49e87d43083a2825
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-14 09:39:07 -0700

    Update MatrixMul.java

--- MatrixMul.java
+++ MatrixMul.java
@@ -1,16 +1,15 @@
 package javaprograms;
-
 import java.util.Scanner;
-
 public class Matrixmul 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Scanner s=new Scanner(System.in);
-		System.out.println("enter row size and column size");
+		System.out.println("Enter row size and column size");
 		int r=s.nextInt();
 		int c=s.nextInt();
 		int ar[][]=new int[r][c];
-		System.out.println("enter matrices elements");
+		System.out.println("Enter matrices elements");
 		for(int i=0;i<r;i++)
 		{
 			for(int j=0;j<c;j++)
@@ -28,11 +27,11 @@ public class Matrixmul
 					System.out.println(" ");
 			}
 		}
-		System.out.println("enter row size and column size");
+		System.out.println("Enter row size and column size");
 		int r2=s.nextInt();
 		int c2=s.nextInt();
 		int ar2[][]=new int[r2][c2];
-		System.out.println("enter matrices elements");
+		System.out.println("Enter matrices elements");
 		for(int i=0;i<r2;i++)
 		{
 			for(int j=0;j<c2;j++)
@@ -50,7 +49,7 @@ public class Matrixmul
 					System.out.println(" ");
 			}
 		}
-		System.out.println("matrix multiplication is");
+		System.out.println("Matrix multiplication is");
 		{
 			
 			int mat3[][]=new int[r][c2];
