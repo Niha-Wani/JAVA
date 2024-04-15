d89fe6be3909097f48aeb7d7f09871014e0c81da
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-15 06:30:27 -0700

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -2,34 +2,34 @@ package javaprograms;
 import java.util.Scanner;
 public class Switch 
 {
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the month number");
-		int x=sc.nextInt();
-		switch(x)
-		{
-		case 1:
-		case 2:
-		case 3:
-		System.out.println("Spring Season");
-		break;
-		case 4:
-		case 5:
-    	        case 6:
-		System.out.println("Summer Season");
-		break;
-		case 7:
-		case 8:
-		case 9:
-		System.out.println("Autumn Season");
-		break;
-		case 10:
-		case 11:
-		case 12:
-		System.out.println("Winter Season");
-	        break;
-	        default:System.out.println("Invalid Number");
-		}
-	}
+      public static void main(String[] args)
+      {
+	  Scanner sc=new Scanner(System.in);
+	  System.out.println("Enter the month number");
+	  int x=sc.nextInt();
+	  switch(x)
+	 {
+	     case 1:
+	     case 2:
+	     case 3:
+	     System.out.println("Spring Season");
+	     break;
+	     case 4:
+	     case 5:
+    	     case 6:
+             System.out.println("Summer Season");
+	     break;
+	     case 7:
+	     case 8:
+	     case 9:
+	     System.out.println("Autumn Season");
+	     break;
+	     case 10:
+	     case 11:
+	     case 12:
+	     System.out.println("Winter Season");
+	     break;
+	     default:System.out.println("Invalid Number");
+	 }
+     }
 }
