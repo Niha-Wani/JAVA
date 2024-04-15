c74eeb3347300a00d97e895637e27586696b1bba
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-15 06:01:14 -0700

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -2,20 +2,20 @@ package javaprograms;
 import java.util.*;
 public class Fact
 {
-	static int fact;
-        static int factorial(int num)
-{
-    	if(num==1)
-    	return 1;
-    	else
-	return fact=num*factorial(num-1);
-}	
-	public static void main(String args[])	
-	{
+   static int fact;
+   static int factorial(int num)
+   {
+      if(num==1)
+      return 1;
+      else
+      return fact=num*factorial(num-1);
+    }	
+    public static void main(String args[])	
+     {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter the desired number");
 	int n=sc.nextInt();
 	int facto=factorial(n);
 	System.out.println("Factorial of : " +n+ " is: "+facto);
-	}
-}
+     }
+ }
