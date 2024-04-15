094624b40713c5f425ce2021e917252770ed51dd
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-15 06:26:35 -0700

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -4,13 +4,13 @@ public class Fact
 {
    static int fact;
    static int factorial(int num)
-   {
-      if(num==1)
-      return 1;
-      else
-      return fact=num*factorial(num-1);
-    }	
-    public static void main(String args[])	
+     {
+        if(num==1)
+        return 1;
+        else
+        return fact=num*factorial(num-1);
+      }	
+       public static void main(String args[])	
      {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter the desired number");
