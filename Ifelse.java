6716121f6bc4bdeca8f33994d0062e097e795aeb
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-17 10:25:04 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,16 @@
+class Ifelse
+ {
+  public static void main(String[] args)
+ {
+    int n = 19;
+ if(n%2== 0)
+ {
+      System.out.println("The number is even.");
+ }
+else
+ {
+      System.out.println("The number is odd.");
+ }
+
+  }
+}
