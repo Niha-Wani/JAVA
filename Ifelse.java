af1dcd32b556742db323c6ff2e4842a2d801c1c6
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-16 22:06:07 -0700

    Update Ifelse.java

--- Ifelse.java
+++ Ifelse.java
@@ -6,7 +6,7 @@ class Ifelse
   {
       Scanner s=new Scanner(System.in);
       System.out.println("Enter the number ");
-      int n=s.
+      int n=s.nextInt();
       if(n%2== 0)
       {
       System.out.println("The number is even.");
