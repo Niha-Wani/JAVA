4066980035917167e6a882a020709068f567e9e0
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-16 21:56:23 -0700

    Update Ifelse.java

--- Ifelse.java
+++ Ifelse.java
@@ -1,16 +1,15 @@
 class Ifelse
- {
+{
   public static void main(String[] args)
- {
-    int n = 19;
- if(n%2== 0)
- {
+  {
+      int n = 19;
+      if(n%2== 0)
+      {
       System.out.println("The number is even.");
- }
-else
- {
+      }
+      else
+      {
       System.out.println("The number is odd.");
- }
-
-  }
+      }
+   }
 }
