df3434d0d9d6295c5f561379b40e2a2e44f0806f
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-18 09:13:37 +0530

    Add files via upload

---
+++ castingtype.java
@@ -0,0 +1,16 @@
+class conversion{
+    public staic void main(string args[])
+    byte b;
+    int i=257;
+    double d=323142;
+    System.out.println("conversion of int to byte:");
+    b=(byte);
+    Sytem.out.println("value of i and b"+i,+b);
+    System.out.println("conversion of int to double:");
+    dl=i;
+    System.out.println("conversion of double to int");
+    i=(int b);
+    System.out.println("d and i"+d,+i);
+    d=(byte);
+    System.out.println("conversion of double to byte");
+}
