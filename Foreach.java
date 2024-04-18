34d4029857d3ad81bf6d6f7141e619b86967ed86
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-18 22:58:26 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,23 @@
+package javaprograms;
+import java.util.*;
+public class Foreach {
+public static void main(String args[]) {
+	int n;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the number of elements ");
+	n=sc.nextInt();
+	int a[]=new int[n];
+	System.out.println("Enter array elements ");
+	for(int i:a) {
+		a[i]=sc.nextInt();
+	}
+	/*System.out.println("The array elements are ");
+	for(int i:a) {
+		System.out.println(i);
+	}*/
+	System.out.println("The array elements are ");
+	for(int i=0;i<n;i++) {
+		System.out.println(a[i]);
+	}
+}
+}
