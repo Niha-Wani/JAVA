73df0eff6ee308f4b63c67ec5dcc2bfd577b8abe
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-18 23:00:15 +0530

    Add files via upload

---
+++ HCF.java
@@ -0,0 +1,17 @@
+package javaprograms;
+import java.util.*;
+public class HCF {
+    public static void main(String args[]) {
+    	int a,b,i,hcf=0;
+    	Scanner sc=new Scanner (System.in);
+    	System.out.println("Enter the first number");
+    	a=sc.nextInt();
+    	System.out.println("Enter the second number");
+    	b=sc.nextInt();
+    	for(i=1;i<=a||i<=b;i++) {
+    		if(a%i==0 && b%i==0)
+    			hcf=i;
+    	}
+    	System.out.println("HCF of two numbers " +a+ " and "+b+ " is "+hcf);
+    }
+}
