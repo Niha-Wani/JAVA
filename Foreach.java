868f9d1e209675c0ae13a761c2bbb7d968208473
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-18 10:29:26 -0700

    Update Foreach.java

--- Foreach.java
+++ Foreach.java
@@ -1,22 +1,27 @@
 package javaprograms;
 import java.util.*;
-public class Foreach {
-public static void main(String args[]) {
+public class Foreach 
+{
+public static void main(String args[]) 
+	{
 	int n;
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter the number of elements ");
 	n=sc.nextInt();
 	int a[]=new int[n];
 	System.out.println("Enter array elements ");
-	for(int i:a) {
+	for(int i:a) 
+	{
 		a[i]=sc.nextInt();
 	}
 	/*System.out.println("The array elements are ");
-	for(int i:a) {
+	for(int i:a)
+       {
 		System.out.println(i);
 	}*/
 	System.out.println("The array elements are ");
-	for(int i=0;i<n;i++) {
+	for(int i=0;i<n;i++) 
+	{
 		System.out.println(a[i]);
 	}
 }
