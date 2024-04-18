f291f959caea1934a775d0a574309a2ae242719c
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-17 20:49:07 -0700

    Update castingtype.java

--- castingtype.java
+++ castingtype.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-class conversion
+class Conversion
 {
     public staic void main(string args[])
     {
-    byte b;
-    int i=257;
-    double d=323142;
-    System.out.println("conversion of int to byte:");
-    b=(byte);
-    Sytem.out.println("value of i and b "+i+b);
-    System.out.println("conversion of int to double:");
-    d=i;
-    System.out.println("conversion of double to int");
-    i=(int b);
-    System.out.println("d and i"+d,+i);
-    d=(byte);
-    System.out.println("conversion of double to byte");
-   }
+      byte b;
+      int i=257;
+      double d=323142;
+      System.out.println("conversion of int to byte:");
+      b=(byte);
+      Sytem.out.println("value of i and b "+i+b);
+      System.out.println("conversion of int to double:");
+      d=i;
+      System.out.println("conversion of double to int");
+      i=(int b);
+      System.out.println("d and i"+d,+i);
+      d=(byte);
+      System.out.println("conversion of double to byte");
+    }
 }
