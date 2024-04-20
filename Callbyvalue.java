e8d040d4d2c851c2e25cba6cafbadd1484e0d858
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-19 22:53:11 -0700

    Update Callbyvalue.java

--- Callbyvalue.java
+++ Callbyvalue.java
@@ -1,17 +1,20 @@
 package javaprograms;
 import java.util.*;
-class Test{
-	void meth(int i,int j) {
+class Test
+{
+	void meth(int i,int j)
+	{
 		i=i*10;
 		j=j+2;
 	}
 }
-public class Callbyvalue {
-public static void main(String args[]) {
-	int a=10, b=20;
-	Test obj=new Test();
-	obj.meth(a, b);
-	System.out.println("Value of a= " +a+ " b= " +b);
-	
-}
-}
+public class Callbyvalue
+	{
+         public static void main(String args[]) 
+		{
+	          int a=10, b=20;
+	          Test obj=new Test();
+	          obj.meth(a, b);
+	          System.out.println("Value of a= " +a+ " b= " +b);
+                }
+        }
