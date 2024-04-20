8463b8abf821027fc59167de99e5ff8d5bdee213
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-19 22:55:33 -0700

    Update Lcm.java

--- Lcm.java
+++ Lcm.java
@@ -1,18 +1,21 @@
 package javaprograms;
 import java.util.Scanner;
-public class Lcm {
-	public static void main(String args[]) {
+public class Lcm 
+{
+	public static void main(String args[]) 
+	{
     	int a,b,i,hcf=0;
     	Scanner sc=new Scanner (System.in);
     	System.out.println("Enter the first number");
     	a=sc.nextInt();
     	System.out.println("Enter the second number");
     	b=sc.nextInt();
-    	for(i=1;i<=a||i<=b;i++) {
+    	for(i=1;i<=a||i<=b;i++) 
+	{
     		if(a%i==0 && b%i==0)
     			hcf=i;
 	}
     	int lcm=a*b/hcf;
     	System.out.println("LCM = " +lcm);
-}
+        }
 }
