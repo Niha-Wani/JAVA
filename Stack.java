05481dd2effbacd733c867124f081c893b1a665a
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-19 23:11:40 -0700

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,38 +1,49 @@
 package javaprograms;
 import java.util.*;
-class St{
+class St
+{
 	int arr[]=new int[10];
 	int top=-1;
 	/*St()
 	{
 		int top=-1;
 	}*/
-	void push(int item) {
-		if(top==arr.length-1) {
+	void push(int item) 
+	{
+		if(top==arr.length-1)
+		{
 		System.out.println("Stack Overflow");	
 		}
-		else {
+		else 
+		{
 			arr[top]=item;
 			top=top+1;
 		}
 	}
-	void pop() {
-		if(top==-1) {
+	void pop() 
+	{
+		if(top==-1) 
+		{
 			System.out.println("Stack underflow");
 		}
-		else {
+		else 
+		{
 			int x=arr[top];
 			top--;
 		}
 	}
-	void display() {
-		for(i=0;i>arr.length-1;i++) {
+	void display() 
+	{
+		for(i=0;i>arr.length-1;i++) 
+		{
 			System.out.println(arr[i]);
 		}
 	}
 }
-public class Stack {
-public static void main(String args[]) {
+public class Stack 
+{
+   public static void main(String args[]) 
+   {
 	int top=-1;
 	St obj=new St();
 	Scanner sc=new Scanner(System.in);
@@ -40,5 +51,5 @@ public static void main(String args[]) {
 	int item=sc.nextInt();
 	obj.push(item);
 	obj.display();
-}
+   }
 }
