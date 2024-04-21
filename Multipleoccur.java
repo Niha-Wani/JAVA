849aec8e1d0cb85541fb265c89e9628ff4f66672
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-21 10:54:33 -0700

    Update Multipleoccur.java

--- Multipleoccur.java
+++ Multipleoccur.java
@@ -22,15 +22,15 @@ public class Multipleoccur
 		}
 		for(int i=0; i < n; i++)
 		{
-		for(int j=1; j < (n-i); j++)
-		{
-		if(ar[j-1] > ar[j])
-		{
-		int temp = ar[j-1];
-		ar[j-1] = ar[j];
-		ar[j] = temp;
-		}
-		}
+         		for(int j=1; j < (n-i); j++)
+	        	{
+		          if(ar[j-1] > ar[j])
+		          {
+		            int temp = ar[j-1];
+		            ar[j-1] = ar[j];
+		            ar[j] = temp;
+		          }
+		        }
 		}
 		System.out.println(" ");
 		System.out.println("Array After Bubble Sort");
@@ -38,20 +38,22 @@ public class Multipleoccur
 		{
 		System.out.print(ar[i] + " ");
 		}
-		for(int i=0;i<n;i++) {
-			if(ar[i]==ar[i+1]) {
-				for(int k=i+1;k<n-1;k++) {
-					ar[k]=ar[k+1];
-				}
-				n--;
+		for(int i=0;i<n;i++) 
+		{
+		   if(ar[i]==ar[i+1]) 
+		   {
+			for(int k=i+1;k<n-1;k++) 
+			{
+			  ar[k]=ar[k+1];
 			}
-		}
+			n--;
+		    }
+		 }
 		System.out.println("our array after removing duplicate elements is");
 		for(int i=0;i<n;i++)
 		{
 			System.out.print(" ");
 			System.out.print(ar[i]);
-	
 		}
 }
 }
