4d91a2bfc70c679f71a0ddff33095bc63a7960c9
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-24 02:00:58 -0700

    Update Swapmain.java

--- Swapmain.java
+++ Swapmain.java
@@ -1,19 +1,24 @@
 import java.util.*;
-class Swap {
+class Swap 
+{
 	int a,b;
-	Swap(int i,int j){
+	Swap(int i,int j)
+	{
 		a=i;
 		b=j;
 	}
-	void swap(Swap ob) {
+	void swap(Swap ob)
+	{
 	int temp;
 	temp=ob.a;
 	ob.a=ob.b;
 	ob.b=temp;
 	}
 }
-public class Swapmain {
-public static void main(String args[]) {
+public class Swapmain 
+{
+public static void main(String args[])
+	{
 	int i=10,j=20;
 	Swap obj = new Swap(i,j);
 	System.out.println("The value of a and b before swapping is " +obj.a+ " and " +obj.b);
