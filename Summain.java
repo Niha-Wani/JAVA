bf284cc1908d4dd9a94df1d204887df927ddc195
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-24 01:55:49 -0700

    Update Summain.java

--- Summain.java
+++ Summain.java
@@ -1,28 +1,36 @@
 import java.util.*;
-class Sum{
-	int sum(int a,int b) {
+class Sum
+{
+	int sum(int a,int b)
+	{
 		int x=a+b;
 		return x;
 	}
-	int sum(int a,int b,int c) {
+	int sum(int a,int b,int c)
+	{
 		int x=a+b+c;
 		return x;
 	}
-	double sum(double a,double b) {
+	double sum(double a,double b)
+	{
 		double x=a+b;
 		return x;
 	}
-	double sum(float a,float b) {
+	double sum(float a,float b) 
+	{
 		double x=a+b;
 		return x;
 	}
-	double sum(float a,int b) {
+	double sum(float a,int b) 
+	{
 		float x=a+b;
 		return x;
 	}
 }
-public class Summain {
-public static void main(String args[]) {
+public class Summain 
+{
+public static void main(String args[])
+	{
 	Sum obj=new Sum();
 	int x=obj.sum(2, 6);
 	System.out.println("The sum is " +x);
