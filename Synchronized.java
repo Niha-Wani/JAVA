3e9903ebab03cc08ff05212b15d10ef3b02f06af
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-25 09:34:40 -0700

    Update Synchronized.java

--- Synchronized.java
+++ Synchronized.java
@@ -1,44 +1,56 @@
 package javaprograms;
-class Callme implements Runnable{
-	synchronized void call(String S) {
-	System.out.println("[" +S);
-	try {
-		Thread.sleep(1000);
-	}
-	catch(InterruptedException e) {
-		System.out.println(e);
-	}
-	System.out.println("]");
+class Callme implements Runnable
+{
+	synchronized void call(String S) 
+	{
+	   System.out.println("[" +S);
+	   try 
+	   {
+	     Thread.sleep(1000);
+	   }
+	   catch(InterruptedException e) 
+	   {
+	      System.out.println(e);
+	   }
+	   System.out.println("]");
 	}
 }
-class Caller implements Runnable{
+class Caller implements Runnable
+{
 	Thread t;
 	String S;
 	Callme target;
-	Caller(String name,Callme t){
+	Caller(String name,Callme t)
+	{
 		S=name;
 		target=t;
 		t=new Thread(this,"Mythread");
 		t.start();
 	}
-	public void run() {
-		synchronized(target) {
-		target.call(S);
-	}
-}
-public class Synchronized {
-public static void main(String args[]) {
-Callme t=new Callme();
-Caller obj1=new Caller("HELLO",t);
-Caller obj2=new Caller("NIHA",t);
-Caller obj3=new Caller("INJILA",t);
-try {
+	public void run() 
+	{
+		synchronized(target)
+		{
+		  target.call(S);
+	        }
+        }
+public class Synchronized
+{
+    public static void main(String args[]) 
+    {
+      Callme t=new Callme();
+      Caller obj1=new Caller("HELLO",t);
+      Caller obj2=new Caller("NIHA",t);
+      Caller obj3=new Caller("INJILA",t);
+       try 
+      {
 	obj1.t.join();
 	obj2.t.join();
 	obj3.t.join();
-}
-catch(InterruptedException e) {
-	System.out.println(e);
-}
-}
+      }
+       catch(InterruptedException e) 
+	{
+	   System.out.println(e);
+        }
+     }
 }
