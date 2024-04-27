f1cebab0c31ee9a29a838713bb6f9e520d3cffdb
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-27 21:55:31 +0530

    Update Joinmain.java

--- Joinmain.java
+++ Joinmain.java
@@ -5,20 +5,20 @@ class Newth implements Runnable
 	Thread t;
 	Newth(String name)
 	{
-		name=this.name;
-	t=new Thread(this,"MYRULES");
-	System.out.println("thread created "+t);
-	t.start();
-}
+	    name=this.name;
+	    t=new Thread(this,"MYRULES");
+	    System.out.println("Thread created " +t);
+	    t.start();
+         }
 	public void run() 
 	{
-		try 
-			{
-			for(int i=0;i<10;i++)
-				{
-				System.out.println("value of i is " +i);
-				Thread.sleep(1000);
-			}
+	        try 
+		{
+		    for(int i=0;i<10;i++)
+		    {
+			System.out.println("value of i is " +i);
+			Thread.sleep(1000);
+		    }
 		}
 		catch(InterruptedException e)
 		{
@@ -30,21 +30,21 @@ public class Joinmain
 {
 	public static void main(String args[]) 
 	{
-       Newth obj1=new Newth("one");
-       Newth obj2=new Newth("Two");
-       Newth obj3=new Newth("Three");
-       obj1.t.isAlive();
-		try 
-			{
-			obj1.t.join();
-			obj2.t.join();
-			obj3.t.join();
-			}
-		catch(InterruptedException e) 
-			{
-			System.out.println(e);
-		}
+             Newth obj1=new Newth("one");
+             Newth obj2=new Newth("Two");
+             Newth obj3=new Newth("Three");
+             obj1.t.isAlive();
+	     try 
+	     {
+		   obj1.t.join();
+		   obj2.t.join();
+		   obj3.t.join();
+	     }
+	     catch(InterruptedException e) 
+	      {
+		   System.out.println(e);
+	      }
 		obj1.t.isAlive();
 		System.out.println("Mainthread exiting");
 	}
-}
+  }
