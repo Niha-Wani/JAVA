da78d1e11c747f20564013c7fe2db66ebd34ccb5
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-28 12:28:47 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,23 +1,28 @@
 package javaprograms;
 import java.util.*;
-public class Recursion {
-public static void main(String args[]) {
+public class Recursion 
+{
+     public static void main(String args[]) 
+     {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter the length of array ");
 	int n=sc.nextInt();
 	int arr[]=new int[n];
 	System.out.println("Enter the elements ");
-	for(int i=0;i<arr.length;i++) {
-		arr[i]=sc.nextInt();
+	for(int i=0;i<arr.length;i++)
+	{
+	    arr[i]=sc.nextInt();
 	}
 	System.out.println("The elements are ");
 	display(arr,0);
-}
-public static void display(int arr[],int index) {
-	if(index==arr.length) {
-		return;
-	}
+      }
+      public static void display(int arr[],int index) 
+      {
+	if(index==arr.length) 
+	{
+	    return;
+        }
 	System.out.print(arr[index]);
 	display(arr,index+1);
-}
+      }
 }
