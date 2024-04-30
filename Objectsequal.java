aa8a52b54f6e325aa8a3561ad228477498c26468
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-30 20:30:02 +0530

    Update Objectsequal.java

--- Objectsequal.java
+++ Objectsequal.java
@@ -1,20 +1,25 @@
 package javaprograms;
 import java.util.*;
-class Testt{
+class Testt
+{
 	int a,b;
-	Testt(int i,int j){
+	Testt(int i,int j)
+	{
 		a=i;
 		b=j;
 	}
-	boolean equal(Testt obj) {
+	boolean equal(Testt obj) 
+	{
 		if(obj.a==a && obj.b==b)
 			return true;
 		else
 			return false;
 	}
 }
-public class Objectsequal {
-public static void main(String args[]) {
+public class Objectsequal 
+{
+    public static void main(String args[])
+    {
 	Testt t1=new Testt(10,20);
 	Testt t2=new Testt(100,200);
 	Testt t3=new Testt(10,20);
@@ -22,5 +27,5 @@ public static void main(String args[]) {
 	System.out.println(t1.equal(t2));
 	System.out.println(t1.equal(t3));
 	System.out.println(t2.equal(t4));
-}
+     }
 }
