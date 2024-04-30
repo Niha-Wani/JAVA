b25e22def224283d2ec056d31925f32f014c2853
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-30 20:49:48 +0530

    Update Threadmain.java

--- Threadmain.java
+++ Threadmain.java
@@ -1,17 +1,22 @@
 package javaprograms;
 
-public class Threadmain {
-	public static void main(String[] args) {
+public class Threadmain
+{
+	public static void main(String[] args)
+	{
 		Thread t=Thread.currentThread();
 		t.setName("Mainthread");
 		System.out.println("Currentthread="+t);
-		try {
-			for(int i=0;i<5;i++) {
-				System.out.println("value of i=" +i);
-				Thread.sleep(1000);
-			}
+		try 
+		{
+		     for(int i=0;i<5;i++) 
+		     {
+			   System.out.println("value of i=" +i);
+			   Thread.sleep(1000);
+		     }
 		}
-		catch(InterruptedException e) {
+		catch(InterruptedException e)
+		{
 			System.out.println(e);
 		}
 	}
