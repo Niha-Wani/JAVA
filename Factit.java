29a08586cdab6ae4cf9ece07fbd4fe2161a239e1
Niha Mukhtar Wani <niha78781@gmail.com>
2024-05-08 20:32:35 +0530

    Update Factit.java

--- Factit.java
+++ Factit.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class Factit 
 {
    public static void main(String[] args) 
+    {
+	int fac=1;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the value");
+	int a=sc.nextInt();
+	for(int i=1;i<=a;i++)
 	{
-		int fac=1;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the value");
-		int a=sc.nextInt();
-		for(int i=1;i<=a;i++)
-		{
-			fac=i*fac;
-		}
-		System.out.println("The factorial of a number is" +fac);
+	   fac=i*fac;
 	}
+		System.out.println("The factorial of a number is" +fac);
+     }
 }
